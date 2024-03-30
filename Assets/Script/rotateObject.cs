7e4de3cbaa16dfefbaf7e8f17486ef3ce9756206
dickyarya <79236074+DickyArya01@users.noreply.github.com>
2024-03-30 23:35:36 +0700

    fix planet size, position, planet rotate function and canvas orientation

--- Assets/Script/rotateObject.cs
+++ Assets/Script/rotateObject.cs
@@ -7,28 +7,11 @@ public class rotateObject : MonoBehaviour
     public GameObject objectRotate;
 
     public float rotateSpeed = 50f;
-    bool rotateStatus = false;
-
-    //rotate object function
-    public void RotateObject()
-    {
-
-        if (rotateStatus == false)
-        {
-            rotateStatus = true;
-        }
-        else
-        {
-            rotateStatus = false;
-        }
-    }
+    [SerializeField] bool shouldRotate = false;
 
     void Update()
     {
-        if (rotateStatus == true)
-        {
-            //rotate object with speed
+        if (shouldRotate)
             objectRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-        }
     }
 }
---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWMyQlFhRC8vLy8vQUFBQm1hRUplOFVwVVVSR3RhNktvcW1PeFFBQVNxNDUrYVRFNmRwQUhzT3dyRThPY3Y3OXVFU1VFaTVzRVplRzlaR05aU1MraGxBQ21SemF6dThjWnVMSUxxWit1QlRNUi9icVBYcC91YTc3bTFVVXdQeEdBbXNaL1kreUVGU1YyRlQ3aHNSM0x3TDFSQlhPU05UWnlHTzduMXNheFI5cUtoNU9WTC83M1Aza1dSQXNLTG9aMlZjZlJzWnNYUW5LNHR0MExZeVRrYU85dlcxQzVZOUd0NklkaC8xeDdyZUtsNDR4eC84enk5aFRmYVppWG9vb0JNUjhTMjNLNGozTVlDQ0ErR2c4WTV0aDJ1WnRwZGsyWFNFRUhHN1JycUhCdDY5YlBxQUJRL1R2SjM3QmkyUk9qbFg2NFkvNmczODFvMFNNZE9rdS9KeTBxRncvZTgyZ1B4RnZQckhqeS8yekZGKzdtaHNZaGtyVG1HNVY=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
