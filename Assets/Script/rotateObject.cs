3ab94059c8f34f3f5dc0df67ca1ef47acce439a5
DewiMasluchah <162815506+DewiMasluchah@users.noreply.github.com>
2024-04-27 08:29:43 -0700

    nambah rotate

--- Assets/Script/rotateObject.cs
+++ Assets/Script/rotateObject.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class rotateObject : MonoBehaviour
 {
+    internal enum RotateDirection
+    {
+        Right,
+        Left,
+    }
+
     public GameObject objectRotate;
 
     public float rotateSpeed = 50f;
+    [SerializeField] RotateDirection rotateDirection = RotateDirection.Right;
     [SerializeField] bool shouldRotate = false;
+
+    int rotateDirValue = 1;
+
+    void Start()
+    {
+        if(rotateDirection == RotateDirection.Left)
+            rotateDirValue = -rotateDirValue;
+    }
 
     void Update()
     {
         if (shouldRotate)
-            objectRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+            objectRotate.transform.Rotate(transform.up, rotateSpeed * Time.deltaTime * rotateDirValue);
     }
 }
--- Assets/video.cs
+++ Assets/video.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class video : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public VideoPlayer _video;
+
+    public void mainkan()
     {
-        
+        _video.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void berhenti()
     {
-        
+        _video.Stop();
+    }
+
+    public void delay()
+    {
+        _video.Pause();
     }
 }
